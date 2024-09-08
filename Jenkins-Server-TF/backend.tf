812a28c9b5b901b98004a3f77c0db57e2df338a1
borayerramnaidu <122146772+borayerramnaidu@users.noreply.github.com>
2024-09-08 09:47:26 +0530

    Update backend.tf

--- Jenkins-Server-TF/backend.tf
+++ Jenkins-Server-TF/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "my-ews-baket1"
+    bucket         = "yerram-ani"
     region         = "us-east-1"
     key            = "End-to-End-Kubernetes-Three-Tier-DevSecOps-Project/Jenkins-Server-TF/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
+    dynamodb_table = "yerramta"
     encrypt        = true
   }
   required_version = ">=0.13.0"
